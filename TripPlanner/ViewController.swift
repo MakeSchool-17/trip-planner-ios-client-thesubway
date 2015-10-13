750e796ed5d97c7058bca1121ab9c2457f6ee763
Daniel Hoang <thesubway@gmail.com>
2015-10-12 21:45:07 -0700

    Add xcode project.

---
+++ TripPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripPlanner
+//
+//  Created by Dan Hoang on 10/12/15.
+//  Copyright © 2015 Dan Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
